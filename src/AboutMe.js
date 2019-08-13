6cdd653c8bb5e898c78f380c133cb26771a94dfb
TrevMike <preince560m@gmail.com>
2019-08-13 12:08:43 -0500

    setting up the site about me

---
+++ src/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Roses from './assects/Roses.jpg'
+import './App.css';
+
+function AboutMe() {
+    return(
+        <div className="aboutMeContainer">
+            {/* {Flores_floral_Roses} */}
+            {/* <img src={Roses} alt="A cluster of roses." className="roseImage"></img> */}
+        </div>
+    )
+}
+
+export default AboutMe
--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import AboutMe from './AboutMe';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="waves-effect waves-light btn">button</div>
+    <div>
+      <header>
+        <div className="navbar-fixed">
+          <nav>
+            <div className="nav-wrapper cyan lighten-4">
+              <div className="brand-logo black-text">Flores Floral and Nursury</div>
+              <ul className="right hide-on-med-and-down">
+                <li><a href="sass.html" className="black-text">Home</a></li>
+                <li><a href="badges.html" className="black-text">About</a></li>
+                <li><a href="badges.html" className="black-text">Portfolio</a></li>
+                <li><a href="badges.html" className="black-text">Contact</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
       </header>
+
+      <AboutMe/>
+
+      <footer className="page-footer ">
+          <div className="container">
+            <div className="row">
+              <div className="col l6 s12">
+                <h5 className="-text">Like what you see?</h5>
+                <p className="grey-text text-lighten-4">Then give us a call and we'll chat through what you need. <br/>
+                We've got the perfect flowers for the occasion.
+                </p>
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">Mobile: (956) 580-8186</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Email: floresfloral386@gmail.com</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Address: 916 Expressway 83 La Joya, Texas</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Address: 916 Expressway 83 La Joya, Texas</a></li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div className="container">
+            © 2017 Copyright
+            <a className="grey-text text-lighten-4 right" href="#!">FaceBook</a>
+            </div>
+          </div>
+        </footer>
     </div>
   );
 }
